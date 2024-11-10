c8254fa80a3e5239d72a4f4745d48b452d2aba2d
SomeLazyGuy <85708571+SomeLazyGuy@users.noreply.github.com>
2024-11-10 21:36:50 +0100

    Merged 2023 from my other repository

---
+++ 2023/includes.h
@@ -0,0 +1,20 @@
+//
+// Created by Konstantin Kilbel on 05.12.23.
+//
+
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::map;
+using std::pair;
+
+#endif //INCLUDES_H
